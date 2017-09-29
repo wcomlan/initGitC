5b7c33d7178d7e4e9256fc639154d82fe3df0fa1
comlan <comlan@localhost.localdomain>
2017-09-29 05:34:16 +0200

    avance

--- hello.c
+++ hello.c
@@ -4,7 +4,24 @@
 
 int main(int argc, char **argv){
 
-	printf("hello World.... et puis c'est tout\n");
+	printf("hello World.... et puis c'est toutzz!\n");
+
+int inc = 0;
+
+	printf("%d arguments\n", argc);
+
+	for(inc = 0; inc < argc; inc++){
+		if (inc == 0){
+			printf("le nom de la commande est %s\n",*argv);
+		}else{
+			printf("le nom de l'argument n°%d est %s\n", inc, *argv);
+		}			
+		
+		argv++;
+		
+	}
+
+	printf("Fin\n");
 
 	return 0;
 
