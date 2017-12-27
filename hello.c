d7e27b7b0d6a9447901f3f410cea4f22409155f5
comlan <comlan@localhost.localdomain>
2017-12-27 17:59:00 +0100

    commit 27/12/2017

--- hello.c
+++ hello.c
@@ -1,27 +1,92 @@
 
 
 #include <stdio.h>
+#include <string.h>
+
+#define NB_ARG	3
 
 int main(int argc, char **argv){
 
 	printf("hello World.... et puis c'est toutzz!\n");
 
-int inc = 0;
+//int inc = 0;
+
+char str_cmd_arg[13] ="";
+char * filename = 0;
+FILE *filestream, *dest_stream;
+int tmp_char;
 
 	printf("%d arguments\n", argc);
 
-	for(inc = 0; inc < argc; inc++){
-		if (inc == 0){
-			printf("le nom de la commande est %s\n",*argv);
+	
+	if(argc == NB_ARG){
+		/* la commande a deux paramètres => argc==4 => OK */
+		strncpy(str_cmd_arg, argv[1], 12);
+		
+		if(strcmp(str_cmd_arg, "--unix_file") == 0){
+		
+			puts("unix file \n");
+			filename = argv[2];
+			puts(filename);
+			puts("\n");
+			
+			filestream = fopen(filename, "r+");
+			
+			if(filestream != NULL){
+			
+				if( (dest_stream = fopen("fichier_dos", "w+")) != NULL){
+					
+					while( (tmp_char = fgetc(filestream)) != EOF){
+					
+					if(tmp_char == '\n'){
+						//fseek(filestream, -1, SEEK_CUR);
+						fputc('\r', dest_stream);
+						fputc(tmp_char, dest_stream);
+					}else{
+						fputc(tmp_char, dest_stream);
+					}
+		
+				}
+					
+					fclose(dest_stream);
+				}else{
+					fclose(dest_stream);
+				}
+			
+				while( (tmp_char = fgetc(filestream)) != EOF){
+					if(tmp_char == 'n'){
+						//fseek(filestream, -1, SEEK_CUR);
+						putchar('r');
+						putchar('n');
+					}
+		
+				}
+	
+				fclose(filestream);
+				
+			}else{
+				fclose(filestream);
+			}
+			
+			
+		}else if(strcmp(str_cmd_arg, "--dos_file") == 0){
+			puts("--dos file \n");
+			filename = argv[2];
+			puts(filename);
+			puts("\n");
+			
+			
+			
 		}else{
-			printf("le nom de l'argument n°%d est %s\n", inc, *argv);
-		}			
-		
-		argv++;
-		
+			puts("bad command's parameter. Expected argument is \"--unix_file\" or \"--dos file\"\n");
+		}
+	}else{
+		/* la commande devrait avoir deux paramètres => argc==4 */
+		puts("\nUnrecognized command line option \n");
 	}
 
-	printf("Fin\n");
+	printf("\nFin\n");
+
 
 	return 0;
 
